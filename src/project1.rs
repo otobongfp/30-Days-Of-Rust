432650c555a85b384e2127ef141db3b133b5602e
unknown <otobongfp@gmail.com>
2024-02-03 10:26:23 +0100

    updates

--- src/project1.rs
+++ src/project1.rs
@@ -1,10 +1,12 @@
 use std::io;
 
 pub fn counter(){
+    //This is how Rust prints to the cli
     println!("\nINPUT A NUMBER TO COUNTDOWN FROM:");
 
     let mut value = String::new();
 
+    //this is from the standard library for handling inputs
     io::stdin()
         .read_line(&mut value)
         .expect("Here is the input");
